81177e0d7cb365f747e70be38949164a2b4a77fd
Anthony Du <github@anthonydu.com>
2023-10-10 22:26:27 -0700

    More LeetCode

---
+++ leetcode/remove-element.py
@@ -0,0 +1,26 @@
+class Solution:
+    def removeElement(self, nums: list[int], val: int) -> int:
+        while val in nums:
+            nums.remove(val)
+        return len(nums)
+
+
+class Solution:
+    def removeElement(self, nums: list[int], val: int) -> int:
+        while 1:
+            try:
+                nums.remove(val)
+            except:
+                break
+        return len(nums)
+
+
+class Solution:
+    def removeElement(self, nums: list[int], val: int) -> int:
+        i = 0
+        while i < len(nums):
+            if nums[i] == val:
+                del nums[i]
+            else:
+                i += 1
+        return len(nums)
